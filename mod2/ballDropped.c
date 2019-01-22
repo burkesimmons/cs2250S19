1d48b7d5e7f8e8656b4db05031d0cbe17bb9a5ba
Burke Simmons <burkesimmons@mail.weber.edu>
2019-01-21 20:39:15 -0700

    added building logic

--- mod2/ballDropped.c
+++ mod2/ballDropped.c
@@ -3,13 +3,13 @@
  *
  *       Filename:  ballDropped.c
  *
- *    Description:  Get the distance traveled by a ball hen it is dropped
+ *    Description:  Get the distance traveled by a ball when it is dropped
  *    from a building. Inital Velocity = 0.
  *
  *        Version:  1.0
  *        Created:  01/17/2019 09:32:29 AM
  *       Revision:  none
- *       Compiler:  gcc ballDroppedpped.c -o ballDropped -lmed
+ *       Compiler:  gcc ballDroppedpped.c -o ballDropped -lm
  *
  *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
  *   Organization:  WSU
@@ -17,17 +17,30 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <math.h>
 
 // Constants
-
-// Function Prototypes
+const double GRAVITY = 9.8;   // ms^2
+const double BUILDING = 500; // meters
 
 // Main Function
 int main()
 {
-
-    return 0;
-}
+    double time = 0;
+    double distance = 0;
+    double fromGround = 0;
+    // Take user input: time travelled
+    printf("Enter the time in seconds: ");
+    scanf("%lf", &time);
+    // Formula: s = 0.5 g t^2
+    distance = 0.5 * GRAVITY * pow(time, 2);
+    // Display distance
+    printf("After %lf seconds, your ball travelled %lf meters\n", 
+            time, distance);
+    // Distance from the ground in meters
+    fromGround = BUILDING - distance;
+    printf("The ball is %lf meters from the ground\n", fromGround);
+    return 0;}
 // Function Definitions
 
 
