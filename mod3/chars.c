055a0fa4826347b6699660dd76aef55561fb4b8e
Burke Simmons <burkesimmons@mail.weber.edu>
2019-01-22 09:04:09 -0700

    Initial commit

---
+++ mod3/chars.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chars.c
+ *
+ *    Description:  Learn how to use characters
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:53:35 AM
+ *       Revision:  none
+ *       Compiler:  gcc chars.c -o chars.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weight = 178.3;
+    char sex = 'M'; // Use single ticks
+
+    printf("Your age is %d, you weigh %lf pounds and your sex is %c.\n", age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+
