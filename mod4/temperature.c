79bc13f38bda17d3f756f549773276d2b8dca15f
Burke Simmons <burkesimmons@mail.weber.edu>
2019-01-29 09:10:49 -0700

    Poor example of committing code. This code does not work. It has many problems compiling. Needs to be fixed.

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -1,35 +1,60 @@
-/*
- * =====================================================================================
- *
- *       Filename:  float.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  01/15/2019 09:27:17 AM
- *       Revision:  none
- *       Compiler:  gcc float.c -o float
- *
- *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
- *   Organization:  WSU
- *
- * =====================================================================================
- */
+
+ =====================================================================================
+
+       Filename:  temperature.c
+
+    Description:  Program to calculate temperature
+
+        Version:  1.0
+        Created:  01/15/2019 09:27:17 AM
+       Revision:  none
+       Compiler:  gcc temperature.c -o temperature.out
+
+         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+   Organization:  WSU
+
+ =====================================================================================
+
 #include <stdio.h>
 
 // Constants
 
 // Function Prototypes
 
-// Main Function
+// Main Functio%n
 int main()
 {
 
-    float temp = 7.9; // Celcius
-    double f_temp = 0;
-    // For float use the %f placeholder
-    // For doubles use the %lf placeholder
-    printf("The temperature is %f Celsius\n", temp);
+    //float temp = 7.9; // Celcius
+    double in_temp, out_temp;
+    int choice; 
+//1) Ask user which temperature mode theywant to use
+    printf("Welcom to the temperature calculator\n");
+    printf("Please enter yourchoice:\n");
+    printf("\t0 for Celcius or1 forFahrenheit: \n");
+    scanf("%d", &choice);
+//2) Basedonchoice calculate Celcius or Fahrenheit
+    if(choice == 1)
+    {
+       printf("Enter the temperature in Celsius: ");
+       scanf("%lf", &in_temp);
+    //Convert from Fahrenheit to Celsius
+    out_temp = (in_temp - 32 * 9.0/5.0) + 32;
+    printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
+    }
+    else if {(choice == 1)
+       printf("Enter the temperature in Fahrenheit: ");
+       scanf("%lf", &in_temp);
+    //Convert from Fahrenheit to Celsius
+    out_temp = (in_temp - 32 * 9.0/5.0) + 32;
+    printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
+       }
+    else
+    {
+        printf("Sorry, you did not enter 0 or 1\nAdios amigo\n");
+    }
+/*
+        printf("The temperature is %f Celsius\n", temp);
     // Now take user input
     printf("What is the temperature in Ogden now in Celsius: ");
     // use %f to capture float information
@@ -38,7 +63,7 @@ int main()
     //Convert from Celsius to Fahrenheit
     f_temp = (temp * 9/5) +32;
     printf("I see, your temp is %lf in Fahrenheit.\n", f_temp);
-
+*/
     return 0;
 }
 // Function Definitions
