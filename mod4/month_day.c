88cdd2955dbc8cab8e92444bc6e70b053e02eb2d
Burke Simmons <burkesimmons@mail.weber.edu>
2019-01-31 09:19:59 -0700

    initial commit

---
+++ mod4/month_day.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  Take month and day from user, then verify entries are vaid.
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:17:32 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    // 1) Capture user input
+    
+    // 2) User IF to validate month
+    
+    // 3) Use SWITCH to validate day of the month
+    
+    return 0;
+}
+// Function Definitions
+
+
