c0ad5334fca6b3528361cedda6f8bd49e093940b
Burke Simmons <burkesimmons@mail.weber.edu>
2019-02-03 17:22:48 -0700

    Updated description and added space before &c to capture the extra ENTER

--- mod3/scanChars.c
+++ mod3/scanChars.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  scanChars.c
  *
- *    Description:  Doing something
+ *    Description:  Take multiple chars as input
  *
  *        Version:  1.0
  *        Created:  01/22/2019 09:44:54 AM
@@ -29,10 +29,12 @@ int main()
     scanf("%c", &cIn);
     printf("You entered [%c]\n", cIn);
 
+    //Note: When capturing chars, use an extra space
+    //in your scanf function. e.g. " &c"
+    //this will help you capture the extra ENTER key
     
-    char cIn;
-    printf("Enter a character: ");
-    scanf("%c", &cIn);
+    printf("Enter 2 characters: ");
+    scanf(" %c", &cIn);
     printf("You entered [%c]\n", cIn);
 
     return 0;
