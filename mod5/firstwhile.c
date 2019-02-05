a6301a4839a7e9a5917e11863602fbc26bbcd6bb
Burke Simmons <burkesimmons@mail.weber.edu>
2019-02-05 08:41:10 -0700

    Initial commit

---
+++ mod5/firstwhile.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  firstwhile.c
+ *
+ *    Description:  learn while loops
+ *
+ *        Version:  1.0
+ *        Created:  02/05/2019 08:37:04 AM
+ *       Revision:  none
+ *       Compiler:  gcc firstwhile.c -o firstwhile.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+#define MAXCOUNT 4
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    // Task 1: Calculate the average of user input values
+    int count = 0;
+    float grade, avg;
+
+    while(count < MAXCOUNT) {
+        printf("Enter %d hw grade(0-100): "), count);
+    }
+    return 0;
+}
+// Function Definitions
+
+
