70a4731be014f7d886fcf8f0a8ac5a85d3e47fcc
Burke Simmons <burkesimmons@mail.weber.edu>
2019-02-05 09:08:47 -0700

    User inputs MAXCOUNT

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -18,18 +18,25 @@
 #include <stdio.h>
 
 // Constants
-#define MAXCOUNT 4
+
 // Function Prototypes
 
 // Main Function
 int main()
 {
     // Task 1: Calculate the average of user input values
+    // Task 2: Validate user input for 0 to 100 only
+    // Task 3: Ask user how many values they need to enter
     int count = 0;
     float grade, avg;
     float total = 0;
+    int MAXCOUNT = 0;
+    
+    printf("Enter total amout of grade values: \n");
+    scanf("%d", &MAXCOUNT);
 
     while(count < MAXCOUNT) {
+        
         printf("\nEnter %d hw grade(0-100): ", count + 1);
         scanf("%f", &grade);
         if (grade < 0 || grade > 100) {
