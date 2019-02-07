839f9348f5ce667355bffc33ac6bb3f71a93a5eb
Burke Simmons <burkesimmons@mail.weber.edu>
2019-02-07 09:03:18 -0700

    Initial commit

---
+++ mod5/guessNumber.c
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  guessNumber.c
+ *
+ *    Description:  Guess the random number
+ *
+ *        Version:  1.0
+ *        Created:  02/07/2019 08:53:07 AM
+ *       Revision:  none
+ *       Compiler:  gcc guessNumber.c -o guessNumber.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h> // needed for rand()
+// Constants
+#define MAX 3
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int number, guess;
+    guess = rand();
+    //int i; // before C89 you need to define i outside the loop
+    for (int i = 0; i < MAX; i++) {
+        printf("Please guess a number between 0 and 10\n");
+        scanf("%d", &number);
+        if(number == guess) {
+            printf("Congratulations, you got the correct number\n");
+            return 0;
+    }
+    }// end of for loop
+    printf("Sorry, you did not guess the [%d] number\nPlay again\n", 
+            guess);
+
+    return 0;
+}
+// Function Definitions
+
+
