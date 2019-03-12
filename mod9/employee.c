afd4bd384174234dc0f048a9a9466c38be23adfe
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-12 09:42:33 -0600

    Updated with grossPay

--- mod9/employee.c
+++ mod9/employee.c
@@ -25,19 +25,35 @@ typedef struct Employee
     int idNum;
     double payRate;
     double hours;
+    double netPay;
+    double taxRate;
+    double grossPay;
 
 }Employee;
+
+#define TAXRATE1 0.10    //  10%
+#define TAXRATE2 0.25    //  25%
+#define TAXRATE3 0.75    //  75%
+
 // Function Prototypes
 double CalcNet(Employee emp); // pass by value
+double CalcGross(Employee* emp); // pass by reference
 
 // Main Function
 int main(int argc, char* argv[])
-{
-    double netPay = 0;
+{ 
     Employee waldo = {1, 7.50, 20};
-    netPay = CalcNet(waldo);
-    printf("Hi Waldo, your net pay for %lf hours at $%6.2lf rate is $%6.2lf\n",
-            waldo.hours, waldo.payRate, netPay);
+    // Set taxRate for waldo
+    waldo.taxRate = TAXRATE1;
+    waldo.netPay = CalcNet(waldo); // pass by value
+    printf("Hi Waldo, your net pay for %.0lf hours at $%4.2lf rate is $%5.2lf\n",
+            waldo.hours, waldo.payRate, waldo.netPay);
+
+
+    waldo.grossPay = CalcGross(&waldo); // pass by reference
+    printf("Hi Waldo, your gross pay for %.0lf hours at $%4.2lf rate is $%5.2lf\n",
+            waldo.hours, waldo.payRate, waldo.grossPay);
+
     return 0;
 }
 // Function Definitions
@@ -53,5 +69,21 @@ int main(int argc, char* argv[])
  */
 double CalcNet(Employee emp)
 {
-    return emp.payRate * emp.hours;
+    return emp.payRate * emp.hours * (1- emp.taxRate);
+}
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  CalcGross
+ *  Description:  Calculate the gross pay based on payRate and hours
+ *  Param: Employe estructure
+ *  Return: Gross Pay as double
+ * =====================================================================================
+ */
+double CalcGross(Employee* emp) // pass by reference
+{
+    // To access structure members when using a pointer (address)
+    // use the dereference operator ->
+    return emp->payRate * emp->hours;
 }
