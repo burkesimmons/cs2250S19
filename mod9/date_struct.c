52bbbe2a63240930c7a395642a174d1fbf6742ed
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-12 09:09:23 -0600

    First commit

---
+++ mod9/date_struct.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  date_struct.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/12/2019 08:53:28 AM
+ *       Revision:  none
+ *       Compiler:  gcc date_struct.c -o date_struct.out
+ *          Usage:  ./date_struct.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    // Variable type is: 
+
+    return 0;
+}
+// Function Definitions
+
+
