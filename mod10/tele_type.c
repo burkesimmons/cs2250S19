e2f23cd6bc470adbbb39b3623b8e03ce199ba367
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-19 09:27:44 -0600

    Added pointers for the next address

--- mod10/tele_type.c
+++ mod10/tele_type.c
@@ -23,11 +23,13 @@
 #define MAXNAME 30
 #define MAXPHONE 15
 
-typedef struct TeleType
+struct TeleType
 {
     char name[MAXNAME];
     char phoneNum[MAXPHONE];
-}Tele;
+    struct TeleType* nextaddr;  // "should" be the last member/variable of this structure
+};
+typedef struct TeleType Tele;
 
 // Function Prototypes
 
@@ -35,11 +37,15 @@ typedef struct TeleType
 int main(int argc, char* argv[])
 {
     Tele t1 = {"Acme, Junio","(801) 891-2344"};
-    //Tele t2 = {"Martinez, Tino","(801) 891-6969"};
-    //Tele t3 = {"Weber, Waldo","(801) 638-0011"};
+    Tele t2 = {"Martinez, Tino","(801) 891-6969"};
+    Tele t3 = {"Weber, Waldo","(801) 638-0011"};
     Tele* first; // pointer(address) to structure
     
     first = &t1; // get the address of Tele structure
+    t1.nextaddr = &t2; // set the address of the next structure
+    t2.nextaddr = &t3; // set the address of the next structure
+    t3.nextaddr = NULL; // set the address of NULL, lastmember
+
     printf("%s\n%s\n", first->name, first->phoneNum);
 
     return 0;
