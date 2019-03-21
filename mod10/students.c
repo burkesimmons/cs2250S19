d5a9fa4f4d0d90088a539dcdc0264da6cfab007a
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-21 10:21:35 -0600

    Added LinkStudent function and A LOT that I don't understand, but hope to

--- mod10/students.c
+++ mod10/students.c
@@ -26,47 +26,87 @@ typedef struct Student
 {
     char fName[50];
     int idNum;
+    struct Student* next;
 } Student;
 
 // Function Prototypes
-void AddStudent(Student* thisStudent, char name[], int id);
+void AddStudent(Student* thisStudent, char name[], 
+        int id, Student* newStudent);
+void LinkStudent(Student* thisStudent, Student* newStudent);
 void DisplayStudent(const Student* st);
+
 
 // Main Function
 int main(int argc, char* argv[])
 {
     int id = 0;
     char name[MAX] = "";
-    Student st;
+    char ans = 'y';
+    char dummy;
+//    Student st;
+    Student *headRec = NULL;
+    Student *nextRec = NULL;
+    Student *currentRec = NULL;
 
-    printf("Student %d\n", id + 1);
-    id = id + 1;
-    printf("Enter your name:\n");
-    fgets(name, MAX, stdin);
-    name[strlen(name) - 1] = '\0';  // Eliminate EOL character
-    // Save it to a Student structure
-    AddStudent(&st, name, id);
-    DisplayStudent(&st);
-    
+    while(ans == 'y')
+    {
+        // first round headRec == NULL
+        if(currentRec != NULL)
+        {
+            LinkStudent(currentRec, nextRec);
+        }
+        // Allocate the memory for the new record
+        currentRec = (Student*) malloc(sizeof(Student));
 
+        if(id == 0)
+        {
+            headRec = currentRec;
+        }
+        printf("Student %d\n", id + 1);
+        id = id + 1;
+        printf("Enter your name:\n");
+        fgets(name, MAX, stdin);
+        name[strlen(name) - 1] = '\0';  // Eliminate EOL character
+        // Save it to a Student structure
+        AddStudent(headRec, name, id, NULL);
+        //Do you want to enter another record [y|n]
+        printf("Do you want to enter another record? [y|n]");
+        scanf("%c%c", &ans, &dummy);
+        fflush(stdin);
+
+    }
+    // Display ALL student records
+    currentRec = headRec;
+    while(currentRec != NULL)
+    {
+        // DisplayStudent();
+        DisplayStudent(currentRec);
+        // Get the next record
+        currentRec = currentRec->next;
+    }
 
     return 0;
 }
 // Function Definitions
 
-/* 
+/*
  * ===  FUNCTION  ======================================================================
  *         Name:  AddStudent
  *  Description:  Populate a student structure
  * =====================================================================================
  */
-void AddStudent(Student* thisStudent, char name[], int id)
+void AddStudent(Student* thisStudent, char name[], 
+        int id, Student* newStudent)
 {
+    
+
     strcpy(thisStudent->fName,name);
     thisStudent->idNum = id;
+    thisStudent->next = newStudent; // set the address of the next member
+
+    return;
+
 }
-
-
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  DisplayStudent
@@ -77,4 +117,22 @@ void AddStudent(Student* thisStudent, char name[], int id)
 void DisplayStudent(const Student* st)
 {
     printf("Hi [%s] student with id[%d]\n", st->fName, st->idNum);
+    return;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  LinkStudent
+ *  Description:  Connect one node/structure to another
+ * =====================================================================================
+ */
+
+void LinkStudent(Student* thisStudent, Student* newStudent)
+{
+    // Connect the records
+//    Student* tmp = NULL;            // dummy
+//    tmp = thisStudent->next;        // currently should be NULL
+    thisStudent->next = newStudent; // Connect this to next record
+//    newStudent->next = tmp;         // This is the end of the list
+    return;
 }
