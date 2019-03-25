e468a99078f2852b291a03e256666e5ee78f1731
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-25 16:19:14 -0600

    inital commit, barebones, no code

---
+++ zyBooks/8-pointers/8.14WarmupContacts/Contacts.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Contacts.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/25/2019 04:16:12 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  CONTACTS__INC__
+#define  CONTACTS__INC__
+    <+DETAILED+>
+
+#endif /* ----- #ifndef CONTACTS__INC__ ----- */
+
---
+++ zyBooks/8-pointers/8.14WarmupContacts/main.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/25/2019 04:16:12 PM
+ *       Revision:  none
+ *       Compiler:  gcc Contacts.c main.c -Wall -o a.out -lm
+ *          Usage:  ./main.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
