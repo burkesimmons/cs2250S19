420eaa184e6ca3a681fbf85c01d75bcfa0bbfa7a
Burke Simmons <burkesimmons@mail.weber.edu>
2019-03-26 08:59:20 -0600

    Initial skeleton

---
+++ mod8/hw/ItemToPurchase.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ItemToPurchase.h
+ *
+ *    Description:  Item library
+ *
+ *        Version:  1.0
+ *        Created:  03/26/2019 08:45:21 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  ITEMTOPURCHASE__INC__
+#define  ITEMTOPURCHASE__INC_
+
+typedef struct ItemToPurchase_struct
+{
+    char itemName[50];
+    char itemDescription[50];
+    int itemPrice;
+    int itemQuantity;
+} ItemToPurchase;
+
+void MakeItemBlank(ItemToPurchase* item); // by reference
+void PrintItemCost(const ItemToPurchase* item); // by const reference
+void PrintItemDescription(const ItemToPurchase* item); // by const reference
+
+
+#endif /* ----- #ifndef ITEMTOPURCHASE__INC__ ----- */
+
