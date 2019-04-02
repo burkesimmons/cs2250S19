b3f8acd4d57966cdf08e35a733dc28c737f239ce
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-02 09:04:17 -0600

    initial commit

---
+++ mod12/hello.cpp
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/02/2019 08:51:13 AM
+ *       Revision:  none
+ *   Compiler (C):  gcc hello.cpp -o hello.out
+ * Compiler (C++):  g++ hello.cpp -o hello.out:
+ *          Usage:  ./hello.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+
+// For C++ Code
+#include <iostream>
+using namespace std;
+
+
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    int num;
+    // for output use: cout
+    cout << "Hello C++" << endl;
+    cout<< "Enter a number: " << endl;
+    // for input use: cin
+    cin >> num;
+    cout << "You entered: " << num << endl;
+
+    return 0;
+}
+// Function Definitions
+
+
