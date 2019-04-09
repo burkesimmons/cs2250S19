411d036926b24f94a20acba1ad406f1351e8e355
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-09 09:44:17 -0600

    Initial commit.

---
+++ mod13/movie_list.cpp
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  movie_list.cpp
+ *
+ *    Description:  Create a movie list program. Loads information from txt file
+ *                  then, presents user with a menu to interact with data.
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 09:41:40 AM
+ *       Revision:  none
+ * Compiler (C++):  g++ movie_list.cpp Movie.cpp -o movie_list.out:
+ *          Usage:  ./movie_list.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C++ Code
+#include <iostream>
+#include "Movie.h"
+using namespace std;
+
+
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
