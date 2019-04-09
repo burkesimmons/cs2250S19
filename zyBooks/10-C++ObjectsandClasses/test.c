f6e708f62d4b998eb22ffa3bd79f258d367a6e1d
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-09 14:13:42 -0600

    committing to create folder

---
+++ zyBooks/10-C++ObjectsandClasses/test.c
@@ -0,0 +1,45 @@
+/*!
+ *    \file  test.c
+ *   \brief  :w
+ *
+ *  \author  Burke Simmons (), burkesimmons@mail.weber.edu
+ *
+ *  \internal
+ *       Created:  04/09/2019
+ *      Revision:  none
+ *      Compiler:  gcc
+ *  Organization:  WSU
+ *     Copyright:  Copyright (c) 2019, Burke Simmons
+ *
+ *  This source code is released for free distribution under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation.
+ */
+#include <stdio.h>
+/* Function Prototypes */
+
+/* Main Function */
+int main(int argc, char* argv[])
+{
+    <+DETAILED+>
+    return 0;
+}
+/* Function Defenitions */
+/** ============================================================================
+ *  @file   test.c
+ *  @brief  Description
+ *
+ *  @author Full Name
+ *
+ *  @internal
+ *       Created  09.Apr.2019
+ *      Revision  none
+ *      Compiler  gcc
+ *  Organization  Organization
+ *     Copyright  Copyright (c) 2019, Full Name
+ *
+ *  This source code is released for free distribution under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation.
+ * =============================================================================
+ */
+
+
