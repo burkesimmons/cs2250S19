1ede4748758d402a28179014efb6584356331c89
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-22 13:33:47 -0600

    <cstring> header problem locally

--- zyBooks/xtracredit/parsingStrings.cpp
+++ zyBooks/xtracredit/parsingStrings.cpp
@@ -18,16 +18,37 @@
  * =====================================================================================
  */
 // For C++ Code
+
 #include <iostream>
-#include  <string>
+#include <string>
+#include <cstring>
 using namespace std;
 
 // Main Function
 int main(int argc, char* argv[])
 {
-    string name;
-    cout << "Enter input string:" << end;
-    getline(cin, name);
+        string name;
+        char* stringPos = nullptr;
+        char* stringQuit = nullptr;
+        stringQuit = strchr(name, 'q');
+    while(stringQuit != nullptr)
+    {
+        cout << "Enter input string:" << endl;
+        getline(cin, name);
+        stringPos ==strchr(name, ',');
+        while(stringPos == nullptr) 
+                && (stringQuit != nullptr)
+        {
+            cout << "Error: No comma in string" << endl;
+            cout << "Enter input string:" << endl;
+            getline(cin, name);
+
+        }
+        if(stringQuit == nullptr)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
 
