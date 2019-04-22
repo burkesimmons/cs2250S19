af808f2572ea66b2d49e35b0c58174ebb25ee9e3
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-22 11:47:43 -0600

    inital commit

---
+++ zyBooks/xtracredit/parsingStrings.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  parsingStrings.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/22/2019 11:41:56 AM
+ *       Revision:  none
+ *   Compiler (C):  gcc parsingStrings.cpp -o parsingStrings.out
+ * Compiler (C++):  g++ parsingStrings.cpp -o parsingStrings.out:
+ *          Usage:  ./parsingStrings.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C++ Code
+#include <iostream>
+#include  <string>
+using namespace std;
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    string name;
+    cout << "Enter input string:" << end;
+    getline(cin, name);
+    return 0;
+}
+
+
+
