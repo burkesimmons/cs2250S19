c4e228ba17b5fc417377f0652f6e2b67649f23f0
Burke Simmons <burkesimmons@mail.weber.edu>
2019-04-23 07:43:03 -0600

    initial commit

---
+++ final/First.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  First.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/23/2019 07:41:24 AM
+ *       Revision:  none
+ *   Compiler (C):  gcc First.cpp -o First.out
+ *          Usage:  ./First.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C Code
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ final/Second.cpp
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Second.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/23/2019 07:41:34 AM
+ *       Revision:  none
+ * Compiler (C++):  g++ Second.cpp -o Second.out:
+ *          Usage:  ./Second.out
+ *
+ *         Author:  Burke Simmons (), burkesimmons@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C++ Code
+#include <iostream>
+using namespace std;
+
+
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
